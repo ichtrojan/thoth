8068618996dab6e49f5f2452011c078fecfd230a
ichtrojan <ordrizzy@gmail.com>
2020-01-05 14:48:49 +0100

    Thoth example

---
+++ example/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ichtrojan/thoth"
+	"log"
+	"net/http"
+)
+
+func main() {
+	logger := thoth.Init()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "Hello, Testing from Thoth")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Endpoint served")
+	})
+
+	if err := http.ListenAndServe(":8888", nil); err != nil {
+		logger.Log(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ichtrojan/thoth
+
+go 1.13
