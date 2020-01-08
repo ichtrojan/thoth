9fa216f363880e589327f1f000d0fbf0c59a906d
ichtrojan <ordrizzy@gmail.com>
2020-01-08 11:20:48 +0100

    Refactor for extention

--- thoth.go
+++ thoth.go
@@ -2,6 +2,7 @@ package thoth
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -12,20 +13,29 @@ const directory = "logs"
 
 type Config struct {
 	directory string
+	filetype  string
 }
 
-func Init(filetype string) Config {
+func Init(filetype string) (Config, error) {
+	var config Config
+
 	if _, err := os.Stat(directory); os.IsNotExist(err) {
 		err = os.MkdirAll(directory, 0755)
 
 		if err != nil {
-			fmt.Println(err)
+			return config, err
 		}
 	}
 
-	var filename = "error.log"
-	if filetype == "json" {
+	var filename string
+
+	switch filetype {
+	case "log":
+		filename = "error.log"
+	case "json":
 		filename = "error.json"
+	default:
+		return config, errors.New("adapter not defined")
 	}
 
 	path := fmt.Sprintf("%s/%s", directory, filename)
@@ -42,16 +52,27 @@ func Init(filetype string) Config {
 		defer file.Close()
 	}
 
-	return Config{directory: path}
+	return Config{directory: path, filetype: filetype}, nil
 }
 
 func (config Config) Log(error error) {
+	switch config.filetype {
+	case "log":
+		_ = config.logFile(error)
+	case "json":
+		_ = config.logJson(error)
+	default:
+		return
+	}
+}
+
+func (config Config) logFile(error error) error {
 	path := config.directory
 
 	var file, err = os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0644)
 
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 
 	defer file.Close()
@@ -61,37 +82,45 @@ func (config Config) Log(error error) {
 	_, err = fmt.Fprintln(file, newError)
 
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 
 	err = file.Sync()
 
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 
-	return
+	return nil
 }
 
-func (config Config) LogJson(error error) {
+func (config Config) logJson(error error) error {
 	path := config.directory
 
 	var file, err = ioutil.ReadFile(path)
 
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
-	jsonData := []map[string]interface{}{}
-	json.Unmarshal(file, &jsonData)
+
+	var jsonData []map[string]interface{}
+
+	_ = json.Unmarshal(file, &jsonData)
 
 	newError := map[string]interface{}{
-		"timestamp": time.Now().Unix(),
+		"timestamp": time.Now().Format("2006-01-02 15:04:05"),
 		"error":     error.Error(),
 	}
 
 	jsonData = append(jsonData, newError)
-	jsonString, _ := json.Marshal(jsonData)
 
-	ioutil.WriteFile(path, jsonString, os.ModePerm)
-	return
+	jsonString, err := json.Marshal(jsonData)
+
+	if err != nil {
+		return err
+	}
+
+	_ = ioutil.WriteFile(path, jsonString, os.ModePerm)
+
+	return nil
 }
