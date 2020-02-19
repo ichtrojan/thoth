9005711fde6003930120ba96288928ff7d179140
elvis-chuks <celvischuks@gmail.com>
2020-02-19 11:43:39 +0100

    added logtail feature

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ichtrojan/thoth
 
 go 1.13
+
+require github.com/gorilla/websocket v1.4.1
--- thoth.go
+++ thoth.go
@@ -7,7 +7,16 @@ import (
 	"io/ioutil"
 	"os"
 	"time"
+
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/websocket"
 )
+
+
 
 const directory = "logs"
 
@@ -54,6 +63,18 @@ func Init(filetype string) (Config, error) {
 
 	return Config{directory: path, filetype: filetype}, nil
 }
+
+
+func (config Config) Tail() error{
+
+	filename = config.directory
+
+	http.HandleFunc("/thoth", serveHome)
+	http.HandleFunc("/ws", serveWs)
+
+	return nil
+}
+
 
 func (config Config) Log(error error) {
 	switch config.filetype {
@@ -124,3 +145,231 @@ func (config Config) logJson(error error) error {
 
 	return nil
 }
+
+
+
+// write logic for websocket here
+
+
+const (
+	// Time allowed to write the file to the client.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the client.
+	pongWait = 60 * time.Second
+
+	// Send pings to client with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+
+	// Poll file for changes with this period.
+	filePeriod = 1 * time.Second
+)
+
+var (
+	// look at addr later
+	// addr      = flag.String("addr", ":8080", "http service address")
+	homeTempl = template.Must(template.New("").Parse(homeHTML))
+	filename  string
+	upgrader  = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+)
+
+func readFileIfModified(lastMod time.Time) ([]byte, time.Time, error) {
+	fi, err := os.Stat(filename)
+	if err != nil {
+		return nil, lastMod, err
+	}
+	if !fi.ModTime().After(lastMod) {
+		return nil, lastMod, nil
+	}
+	p, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, fi.ModTime(), err
+	}
+	return p, fi.ModTime(), nil
+}
+
+func reader(ws *websocket.Conn) {
+	defer ws.Close()
+	ws.SetReadLimit(512)
+	ws.SetReadDeadline(time.Now().Add(pongWait))
+	ws.SetPongHandler(func(string) error { ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	for {
+		_, _, err := ws.ReadMessage()
+		if err != nil {
+			break
+		}
+	}
+}
+
+func writer(ws *websocket.Conn, lastMod time.Time) {
+	lastError := ""
+	pingTicker := time.NewTicker(pingPeriod)
+	fileTicker := time.NewTicker(filePeriod)
+	defer func() {
+		pingTicker.Stop()
+		fileTicker.Stop()
+		ws.Close()
+	}()
+	for {
+		select {
+		case <-fileTicker.C:
+			var p []byte
+			var err error
+
+			p, lastMod, err = readFileIfModified(lastMod)
+
+			if err != nil {
+				if s := err.Error(); s != lastError {
+					lastError = s
+					p = []byte(lastError)
+				}
+			} else {
+				lastError = ""
+			}
+
+			if p != nil {
+				ws.SetWriteDeadline(time.Now().Add(writeWait))
+				if err := ws.WriteMessage(websocket.TextMessage, p); err != nil {
+					return
+				}
+			}
+		case <-pingTicker.C:
+			ws.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := ws.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+				return
+			}
+		}
+	}
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		if _, ok := err.(websocket.HandshakeError); !ok {
+			log.Println(err)
+		}
+		return
+	}
+
+	var lastMod time.Time
+	if n, err := strconv.ParseInt(r.FormValue("lastMod"), 16, 64); err == nil {
+		lastMod = time.Unix(0, n)
+	}
+
+	go writer(ws, lastMod)
+	reader(ws)
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	// if r.URL.Path != "/" {
+	// 	http.Error(w, "Not found", http.StatusNotFound)
+	// 	return
+	// }
+	// if r.Method != "GET" {
+	// 	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	// 	return
+	// }
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	p, lastMod, err := readFileIfModified(time.Time{})
+
+	if err != nil {
+		p = []byte(err.Error())
+		lastMod = time.Unix(0, 0)
+	}
+	var v = struct {
+		Host    string
+		Data    string
+		LastMod string
+	}{
+		r.Host,
+		string(p),
+		strconv.FormatInt(lastMod.UnixNano(), 16),
+	}
+	homeTempl.Execute(w, &v)
+}
+
+
+
+const homeHTML = `<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Thoth Dashboard</title>
+  </head>
+  <body>
+    <div class="wrap">
+        <p><h3>Thoth logs</h3></p>
+        <div class="tool-bar">
+            
+        </div>
+        <div class="card">
+            <div class="card-body ">
+                <pre id="fileData" class="white-text">{{.Data}}</pre>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script type="text/javascript">
+		var passw = prompt("enter secure key");
+		var real = "1234"
+		if(passw == real){
+			(function() {
+				// var seckKey = prompt("enter secure key");
+				// // prolly use this for authentication
+				// console.log(seckKey)
+				var data = document.getElementById("fileData");
+				data.style.color = "white"
+				var conn = new WebSocket("ws://{{.Host}}/ws?lastMod={{.LastMod}}");
+				conn.onclose = function(evt) {
+					data.textContent = 'Connection closed';
+				}
+				conn.onmessage = function(evt) {
+					console.log('file updated');
+					console.log(evt)
+					data.textContent = evt.data;
+				}
+
+			})();
+		}else{
+			alert("invalid login details");
+			var data = document.getElementById("fileData");
+			data.textContent = 'Invalid security key';
+		}
+        
+    </script>
+    
+    <style>
+    
+    .wrap{
+        margin:auto;
+        width:90%;
+    }
+    /* .card{
+        box-shadow: 2px 2px 5px 2px rgba(53, 196, 60, 0.3);
+     }  */
+    .card-body{
+        padding:20px;
+        background-color:black; 
+        color:white;
+    } 
+    </style>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
+`
