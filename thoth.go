aca37485bb119a9b3962921bfa040d2495db489d
ichtrojan <ordrizzy@gmail.com>
2020-03-16 01:07:14 +0100

    Cleanup, Ignore statements explicitly, template parsing.

--- thoth.go
+++ thoth.go
@@ -16,9 +16,9 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-
-
 const directory = "logs"
+
+const dashboardView = "./views/dashboard.gohtml"
 
 type Config struct {
 	directory string
@@ -64,9 +64,7 @@ func Init(filetype string) (Config, error) {
 	return Config{directory: path, filetype: filetype}, nil
 }
 
-
-func (config Config) Serve(endpoint string) error{
-
+func (config Config) Serve(endpoint string) error {
 	filename = config.directory
 
 	http.HandleFunc(endpoint, serveHome)
@@ -74,7 +72,6 @@ func (config Config) Serve(endpoint string) error{
 
 	return nil
 }
-
 
 func (config Config) Log(error error) {
 	switch config.filetype {
@@ -146,8 +143,6 @@ func (config Config) logJson(error error) error {
 	return nil
 }
 
-
-
 const (
 	// Time allowed to write the file to the client.
 	writeWait = 10 * time.Second
@@ -163,10 +158,9 @@ const (
 )
 
 var (
-
-	homeTempl = template.Must(template.New("").Parse(homeHTML))
-	filename  string
-	upgrader  = websocket.Upgrader{
+	homeTempl, _ = template.ParseFiles(dashboardView)
+	filename     string
+	upgrader     = websocket.Upgrader{
 		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
 	}
@@ -189,11 +183,19 @@ func readFileIfModified(lastMod time.Time) ([]byte, time.Time, error) {
 
 func reader(ws *websocket.Conn) {
 	defer ws.Close()
+
 	ws.SetReadLimit(512)
-	ws.SetReadDeadline(time.Now().Add(pongWait))
-	ws.SetPongHandler(func(string) error { ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+
+	_ = ws.SetReadDeadline(time.Now().Add(pongWait))
+
+	ws.SetPongHandler(func(string) error {
+		_ = ws.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
+
 	for {
 		_, _, err := ws.ReadMessage()
+
 		if err != nil {
 			break
 		}
@@ -202,13 +204,17 @@ func reader(ws *websocket.Conn) {
 
 func writer(ws *websocket.Conn, lastMod time.Time) {
 	lastError := ""
+
 	pingTicker := time.NewTicker(pingPeriod)
+
 	fileTicker := time.NewTicker(filePeriod)
+
 	defer func() {
 		pingTicker.Stop()
 		fileTicker.Stop()
-		ws.Close()
+		_ = ws.Close()
 	}()
+
 	for {
 		select {
 		case <-fileTicker.C:
@@ -227,13 +233,15 @@ func writer(ws *websocket.Conn, lastMod time.Time) {
 			}
 
 			if p != nil {
-				ws.SetWriteDeadline(time.Now().Add(writeWait))
+				_ = ws.SetWriteDeadline(time.Now().Add(writeWait))
+
 				if err := ws.WriteMessage(websocket.TextMessage, p); err != nil {
 					return
 				}
 			}
 		case <-pingTicker.C:
-			ws.SetWriteDeadline(time.Now().Add(writeWait))
+			_ = ws.SetWriteDeadline(time.Now().Add(writeWait))
+
 			if err := ws.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
 				return
 			}
@@ -256,11 +264,11 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 	}
 
 	go writer(ws, lastMod)
+
 	reader(ws)
 }
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
-
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 	p, lastMod, err := readFileIfModified(time.Time{})
@@ -278,86 +286,6 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 		string(p),
 		strconv.FormatInt(lastMod.UnixNano(), 16),
 	}
-	homeTempl.Execute(w, &v)
+
+	_ = homeTempl.Execute(w, &v)
 }
-
-
-const homeHTML = `<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <title>Thoth Dashboard</title>
-  </head>
-  <body>
-    <div class="wrap">
-        <p><h3>Thoth logs</h3></p>
-        <div class="tool-bar">
-            
-        </div>
-        <div class="card">
-            <div class="card-body ">
-                <pre id="fileData" class="white-text">{{.Data}}</pre>
-            </div>
-        </div>
-    </div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script type="text/javascript">
-		var passw = prompt("enter secure key");
-		var real = "1234"
-		if(passw == real){
-			(function() {
-				// var seckKey = prompt("enter secure key");
-				// // prolly use this for authentication
-				// console.log(seckKey)
-				var data = document.getElementById("fileData");
-				data.style.color = "white"
-				var conn = new WebSocket("ws://{{.Host}}/ws?lastMod={{.LastMod}}");
-				conn.onclose = function(evt) {
-					data.textContent = 'Connection closed';
-				}
-				conn.onmessage = function(evt) {
-					console.log('file updated');
-					console.log(evt)
-					data.textContent = evt.data;
-				}
-
-			})();
-		}else{
-			alert("invalid login details");
-			var data = document.getElementById("fileData");
-			data.textContent = 'Invalid security key';
-		}
-        
-    </script>
-    
-    <style>
-    
-    .wrap{
-        margin:auto;
-        width:90%;
-    }
-    /* .card{
-        box-shadow: 2px 2px 5px 2px rgba(53, 196, 60, 0.3);
-     }  */
-    .card-body{
-        padding:20px;
-        background-color:black; 
-        color:white;
-    } 
-    </style>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
-
-
-`
