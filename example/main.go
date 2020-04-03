954515f5d7b5955d19d4178bf3e90390970f63b3
ichtrojan <ordrizzy@gmail.com>
2020-04-03 01:31:48 +0100

    Example usage

--- example/main.go
+++ example/main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/ichtrojan/thoth"
 	"log"
 	"net/http"
-	"errors"
-
-	"github.com/ichtrojan/thoth"
-	
 )
 
 func main() {
-
 	json, err := thoth.Init("json")
 
 	if err != nil {
@@ -24,32 +20,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	err = file.Serve("/logs","12345")
-
-	if err != nil {
-		fmt.Println(err)
+	if err := file.Serve("/logs", "12345"); err != nil {
 		json.Log(err)
 		file.Log(err)
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprintf(w, "Hello, Testing from Thoth")
-		
-		
-		if err != nil {
-			json.Log(err)
-			file.Log(err)
-		}
+		_, _ = fmt.Fprintf(w, "Hello, Testing from Thoth")
 	})
 
 	if err := http.ListenAndServe(":8888", nil); err != nil {
-
-		er := errors.New("Starting your server")
-
-		file.Log(er)
-		json.Log(er)
-
-		json.Log(err)
 		file.Log(err)
+		json.Log(err)
 	}
 }
